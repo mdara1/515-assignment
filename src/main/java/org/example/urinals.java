66e8be68e4a7ca69bda4f3b01e57cf4190ce3826
Manoj Dara <mdara1@asu.edu>
2022-10-28 14:40:07 -0700

    modified test cases and urinals.java for multiple string input in a file

--- src/main/java/org/example/urinals.java
+++ src/main/java/org/example/urinals.java
@@ -1,45 +1,56 @@
 package org.example;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /* Author Manoj Dara */
 public class urinals {
-    public  String readInput(String fileName) throws IOException {
+    public  List<String> readInput(String fileName) throws IOException {
+        List<String> list = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String line= bufferedReader.readLine();
-        bufferedReader.close();
-        System.out.println(line + " input string");
-        return line;
-    }
-    public int countUrinals(String string) {
-        int counter = 0;
-        StringBuilder str = new StringBuilder(string);
-        for (int i = 0; i < string.length(); i++) {
-            if(i<str.length()-1) {
-                if (str.charAt(i) == '1' && str.charAt(i + 1) == '1') return -1;
-            }
-            if (str.charAt(i) == '0') {
-                if (i == 0) {
-                    if (str.charAt(i + 1) == '0') {
-                        str.setCharAt(i, '1');
-                        counter++;
-                    }
-                }
-                else if (i == string.length() - 1) {
-                    if (str.charAt(i - 1) == '0') {
-                        str.setCharAt(i, '1');
-                        counter++;
-                    }
-                }
-             else {
-                if (str.charAt(i - 1) == '0' && str.charAt(i + 1) == '0') {
-                    str.setCharAt(i, '1');
-                    counter++;
-                }
-            }
+        String line;
+        while((line = bufferedReader.readLine()) != null){
+            list.add(line);
         }
+        bufferedReader.close();
+        return list;
     }
-       return counter;
+    public List<Integer> countUrinals(List<String> list) {
+        List<Integer> result  = new ArrayList<>();
+        for (Object o : list) {
+            String string = o.toString();
+            int counter = 0;
+            StringBuilder str = new StringBuilder(string);
+            for (int i = 0; i < string.length(); i++) {
+                if (i < str.length() - 1) {
+                    if (str.charAt(i) == '1' && str.charAt(i + 1) == '1') {
+                        counter = -1;
+                        break;
+                    }
+                }
+                if (str.charAt(i) == '0') {
+                    if (i == 0) {
+                        if (str.charAt(i + 1) == '0') {
+                            str.setCharAt(i, '1');
+                            counter++;
+                        }
+                    } else if (i == string.length() - 1) {
+                        if (str.charAt(i - 1) == '0') {
+                            str.setCharAt(i, '1');
+                            counter++;
+                        }
+                    } else {
+                        if (str.charAt(i - 1) == '0' && str.charAt(i + 1) == '0') {
+                            str.setCharAt(i, '1');
+                            counter++;
+                        }
+                    }
+                }
+            }
+            result.add(counter);
+        }
+        return result;
     }
     public static void main(String[] args) throws IOException {
     urinals urinals = new urinals();
--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,19 +15,64 @@ class UrinalsTest {
     public void readInputTestForEmptyFile() throws IOException {
         System.out.println("===== Manoj Dara == TEST ONE EXECUTED =====");
         urinals urinals =  new urinals();
-        String fileName = "urinals.dat" ;
-        boolean actual = Objects.equals(urinals.readInput(fileName), null);
+        String fileName = "urinalsEmpty.dat" ;
+        List<String> fileContent,emptyList = new ArrayList<>();
+        fileContent = urinals.readInput(fileName);
+        emptyList.add("1");
+        emptyList.remove(0);
+        boolean actual = Objects.equals(fileContent, emptyList);
         boolean expected = true;
         assertEquals(expected,actual,"Should return Empty String");
     }
 
     @Test
-    public void countUrinalsTest(){
+    public void countUrinalsTestTwo(){
         System.out.println("===== Manoj Dara == TEST TWO EXECUTED =====");
         urinals urinals = new urinals();
-        String string = "1001";
-        int expected = 0;
-        assertEquals(expected,urinals.countUrinals(string), "Should return 0");
+        List<String> list = new ArrayList<>();
+        list.add("10001");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected,urinals.countUrinals(list), "Should return 1");
     }
-
+    @Test
+    public void countUrinalsTestThree(){
+        System.out.println("===== Manoj Dara == TEST THREE EXECUTED =====");
+        urinals urinals = new urinals();
+        List<String> list = new ArrayList<>();
+        list.add("1001");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        assertEquals(expected,urinals.countUrinals(list), "Should return 0");
+    }
+    @Test
+    public void countUrinalsTestFour(){
+        System.out.println("===== Manoj Dara == TEST FOUR EXECUTED =====");
+        urinals urinals = new urinals();
+        List<String> list = new ArrayList<>();
+        list.add("00000");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(3);
+        assertEquals(expected,urinals.countUrinals(list), "Should return 3");
+    }
+    @Test
+    public void countUrinalsTestFive(){
+        System.out.println("===== Manoj Dara == TEST FIVE EXECUTED =====");
+        urinals urinals = new urinals();
+        List<String> list = new ArrayList<>();
+        list.add("01000");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected,urinals.countUrinals(list), "Should return 1");
+    }
+    @Test
+    public void countUrinalsTestSix(){
+        System.out.println("===== Manoj Dara == TEST SIX EXECUTED =====");
+        urinals urinals = new urinals();
+        List<String> list = new ArrayList<>();
+        list.add("011");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(-1);
+        assertEquals(expected,urinals.countUrinals(list), "Should return -1");
+    }
 }

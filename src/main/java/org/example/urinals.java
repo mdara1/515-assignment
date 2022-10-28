0336eab3430cb4683aac3f151ee11dc6e93aae5d
Manoj Dara <mdara1@asu.edu>
2022-10-28 11:59:13 -0700

    Test case 1 passed for empty string

--- src/main/java/org/example/urinals.java
+++ src/main/java/org/example/urinals.java
@@ -1,14 +1,16 @@
 package org.example;
 
-import java.io.File;
+import java.io.*;
 
 /* Author Manoj Dara */
 public class urinals {
-    public String readInput(File file){
-        System.out.println("Not yet Implemented");
-        return "tbd";
+    public  String readInput(String fileName) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        System.out.println(bufferedReader.readLine());
+        return bufferedReader.readLine();
     }
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+    urinals urinals = new urinals();
+        urinals.readInput("urinals.dat");
     }
 }
--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class urinalsTest {
 
     @Test
-    public void readInputTestForEmptyFile(){
+    public void readInputTestForEmptyFile() throws IOException {
         System.out.println("===== Manoj Dara == TEST ONE EXECUTED =====");
         urinals urinals =  new urinals();
-        File file = new File("urinals.dat");
-        boolean actual = Objects.equals(urinals.readInput(file), " ");
+        String fileName = "urinals.dat" ;
+        boolean actual = Objects.equals(urinals.readInput(fileName), null);
         boolean expected = true;
         assertEquals(expected,actual,"Should return Empty String");
     }

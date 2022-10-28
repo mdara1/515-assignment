e4d7d7eade30623870090aa052b389a75df2c998
Manoj Dara <mdara1@asu.edu>
2022-10-28 11:44:58 -0700

    Test case 1 failed

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,9 +15,8 @@ class urinalsTest {
         System.out.println("===== Manoj Dara == TEST ONE EXECUTED =====");
         urinals urinals =  new urinals();
         File file = new File("urinals.dat");
-        boolean actual = false;
-        if(urinals.readInput(file) == "") actual = true;
-        boolean expected = false;
+        boolean actual = Objects.equals(urinals.readInput(file), " ");
+        boolean expected = true;
         assertEquals(expected,actual,"Should return Empty String");
     }
 

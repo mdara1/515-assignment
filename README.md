e1f0255d6957a8163f2e1c16e915ec19720a05d0
Manoj Dara <mdara1@asu.edu>
2022-10-27 21:29:52 -0700

    created urinals.java and added description in README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 515-assignment
+
+Problem Statement: To create a Test-Driven Development program for counting urinals 
---
+++ src/urinals.java
@@ -0,0 +1,7 @@
+package src;
+/* Author: Manoj Dara */
+public class urinals {
+    public static void main(String[] args) {
+
+    }
+}

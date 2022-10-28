fe11b179996f24bce904e04aa8efe3064f005ede
Manoj Dara <mdara1@asu.edu>
2022-10-28 16:36:03 -0700

    added test  to check writeOutput

--- src/main/java/org/example/urinals.java
+++ src/main/java/org/example/urinals.java
@@ -52,8 +52,42 @@ public class urinals {
         }
         return result;
     }
+    public void writeOutput(List<Integer> list) throws IOException {
+        File path = new File(".");
+        String fileName = "rule";
+        File [] files = path.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isFile() && file.toString().startsWith("./" + fileName)) {
+                fileName = file.toString().replace("./", "");
+                break;
+            }
+        }
+        String fileToWriteTo;
+        if(fileName.equals("rule")) fileToWriteTo = "rule.txt";
+        else{
+            File oldFile = new File(fileName);
+            System.out.println(oldFile.delete());
+            int start = fileName.indexOf("e");
+            int end = fileName.indexOf(".");
+            String subString = fileName.substring(start + 1, end);
+            if(subString.isEmpty()) fileToWriteTo = "rule1.txt";
+            else{
+                int count = Integer.parseInt(subString);
+                count = count+1;
+                fileToWriteTo = "rule" + count +".txt";
+            }
+        }
+        System.out.println("Writing to File " +fileToWriteTo);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWriteTo));
+        for (Integer integer : list) {
+            bufferedWriter.write(integer.toString() + "\n");
+        }
+        bufferedWriter.close();
+    }
     public static void main(String[] args) throws IOException {
     urinals urinals = new urinals();
     System.out.println(urinals.countUrinals(urinals.readInput("urinals.dat")));
+    urinals.writeOutput(urinals.countUrinals(urinals.readInput("urinals.dat")));
     }
 }
--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -74,5 +75,41 @@ class UrinalsTest {
         List<Integer> expected = new ArrayList<>();
         expected.add(-1);
         assertEquals(expected,urinals.countUrinals(list), "Should return -1");
+    }
+    @Test
+    public void writeOutputTest() throws IOException {
+        System.out.println("===== Manoj Dara == TEST SIX EXECUTED =====");
+        urinals urinals = new urinals();
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0);
+        list.add(0);
+        String str = "rule.txt";
+        int counter = 0;
+        while(true){
+            File file = new File(str);
+            if(file.exists()){
+                counter++;
+                str = "rule"+counter+".txt";
+
+            }
+            else break;
+        }
+        String expected;
+        if(counter==0) expected = "rule.txt";
+         else expected = "rule"+counter+".txt";
+        urinals.writeOutput(list);
+        counter = 0;
+        while(true){
+            File file = new File(str);
+            if(file.exists()){
+                counter++;
+                str = "rule"+counter+".txt";
+
+            }
+            else break;
+        }
+        String actual = str;
+        assertEquals(expected,actual);
     }
 }
